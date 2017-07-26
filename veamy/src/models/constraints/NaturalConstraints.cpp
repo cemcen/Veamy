4e95bdaa1ae7183995a02b49b14110c78ba3bee1
Catalina Alvarez <capalvarez.i@gmail.com>
2017-07-26 14:13:50 -0400

    Changes natural constraints to accept non constant conditions

--- veamy/src/models/constraints/NaturalConstraints.cpp
+++ veamy/src/models/constraints/NaturalConstraints.cpp
@@ -12,28 +12,27 @@ Eigen::VectorXd NaturalConstraints::boundaryVector(std::vector<Point> points, Po
         std::vector<Constraint> constraints = segment_map[constrainedInfo.container];
 
         Eigen::MatrixXd Nbar;
-        Nbar = Eigen::MatrixXd::Zero(2,4);
+        Nbar = Eigen::MatrixXd::Zero(2,2);
         Nbar(0,0) = 1.0/2;
         Nbar(1,1) = 1.0/2;
-        Nbar(0,2) = 1.0/2;
-        Nbar(1,3) = 1.0/2;
 
-        std::cout<< Nbar << std::endl << std::endl;
-        Eigen::VectorXd h;
-
-        h = Eigen::VectorXd::Zero(2);
+        Eigen::VectorXd hFirst, hSecond;
+        hFirst = Eigen::VectorXd::Zero(2), hSecond = Eigen::VectorXd::Zero(2);
 
         for(Constraint c: constraints){
-            h(0) += c.getValue(points[segment.getFirst()])*c.isAffected(DOF::Axis::x);
-            h(1) += c.getValue(points[segment.getSecond()])*c.isAffected(DOF::Axis::y);
+            hFirst(0) += c.getValue(points[segment.getFirst()])*c.isAffected(DOF::Axis::x);
+            hFirst(1) += c.getValue(points[segment.getFirst()])*c.isAffected(DOF::Axis::y);
+
+            hSecond(0) += c.getValue(points[segment.getSecond()])*c.isAffected(DOF::Axis::x);
+            hSecond(1) += c.getValue(points[segment.getSecond()])*c.isAffected(DOF::Axis::y);
         }
 
-        std::cout<< h << std::endl << std::endl;
-        std::cout<< Nbar.transpose()*h << std::endl << std::endl;
-
         double length = segment.length(points);
-        Eigen::VectorXd result = length*(Nbar.transpose()*h);
-        std::cout << result << std::endl << std::endl;
+        Eigen::VectorXd resultFirst = length*(Nbar.transpose()*hFirst);
+        Eigen::VectorXd resultSecond = length*(Nbar.transpose()*hSecond);
+
+        Eigen::VectorXd result(resultFirst.rows() + resultSecond.rows());
+        result << resultFirst, resultSecond;
 
         return result;
     }
