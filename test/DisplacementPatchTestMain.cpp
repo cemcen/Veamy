a43b7050b751d8a98d8a5292ff70d7e6651725be
Alejandro Ortiz-Bernardin <aortizb@gmail.com>
2017-08-30 00:41:30 -0300

    Update DisplacementPatchTestMain.cpp

--- test/DisplacementPatchTestMain.cpp
+++ test/DisplacementPatchTestMain.cpp
@@ -1,8 +1,8 @@
-
 #include <mesher/voronoi/TriangleMeshGenerator.h>
 #include <veamy/Veamer.h>
 #include <veamy/models/constraints/values/Function.h>
 #include <mesher/models/generator/functions.h>
+#include <utilities/utilities.h>
 #include <veamy/physics/MaterialPlaneStrain.h>
 
 double uXPatch(double x, double y){
@@ -14,17 +14,38 @@ double uYPatch(double x, double y){
 }
 
 int main(){
+    std::cout << "*** Starting Veamy ***" << std::endl;
+    std::cout << "--> Test: Displacement patch test <--" << std::endl;
+    std::cout << "..." << std::endl;
+
+    // DEFINING PATH FOR THE OUTPUT FILES:
+    // If the path for the output files is not given, they are written to /home directory by default.
+    // Otherwise, include the path. For instance, for /home/user/Documents/Veamy/output.txt , the path
+    // must be "Documents/Veamy/output.txt"
+    // CAUTION: the path must exists either because it is already in your system or becuase it is created
+    // by Veamy's configuration files. For instance, Veamy creates the folder "/test" inside "/build", so
+    // one can save the output files to "/build/test/" folder, but not to "/build/test/mycustom_folder",
+    // since "/mycustom_folder" won't be created by Veamy's configuration files.
+    std::string meshFileName = "Software/Veamy-master/build/test/disp_patch_test_mesh.txt";
+    std::string dispFileName = "Software/Veamy-master/build/test/disp_patch_test_displacements.txt";
+
+    std::cout << "+ Defining the domain ... ";
     std::vector<Point> rectangle4x8_points = {Point(0, -2), Point(8, -2), Point(8, 2), Point(0, 2)};
     Region rectangle4x8(rectangle4x8_points);
-    rectangle4x8.generateSeedPoints(PointGenerator(functions::constantAlternating(), functions::constant()), 24, 12);
+    std::cout << "done" << std::endl;
 
+    std::cout << "+ Generating polygonal mesh ... ";
+    rectangle4x8.generateSeedPoints(PointGenerator(functions::constantAlternating(), functions::constant()), 24, 12);
     std::vector<Point> seeds = rectangle4x8.getSeedPoints();
     TriangleMeshGenerator meshGenerator = TriangleMeshGenerator (seeds, rectangle4x8);
     PolygonalMesh mesh = meshGenerator.getMesh();
-    mesh.printInFile("rectangle4x8ConstantAlternating.txt");
+    std::cout << "done" << std::endl;
 
-    Veamer v;
+    std::cout << "+ Printing mesh to a file ... ";
+    mesh.printInFile(meshFileName);
+    std::cout << "done" << std::endl;
 
+    std::cout << "+ Defining Dirichlet and Neumann boundary conditions ... ";
     EssentialConstraints essential;
     Function* uXConstraint = new Function(uXPatch);
     Function* uYConstraint = new Function(uYPatch);
@@ -55,15 +76,33 @@ int main(){
 
     ConstraintsContainer container;
     container.addConstraints(essential, mesh);
+    std::cout << "done" << std::endl;
 
-    Material* m = new MaterialPlaneStrain (1e7, 0.3);
-    ProblemConditions conditions(container, m);
+    std::cout << "+ Defining linear elastic material ... ";
+    Material* material = new MaterialPlaneStrain (1e7, 0.3);
+    ProblemConditions conditions(container, material);
+    std::cout << "done" << std::endl;
 
+    std::cout << "+ Preparing the simulation ... ";
+    Veamer v;
     v.initProblem(mesh, conditions);
+    std::cout << "done" << std::endl;
 
+    std::cout << "+ Simulating ... ";
     Eigen::VectorXd x = v.simulate(mesh);
-    //By default, the file is created in home/, to create somewhere else, for example, /home/user/Documents/Veamy,
-    //create the fileName variable as "/Documents/Veamy/displacement.txt"
-    std::string fileName = "displacement.txt";
-    v.writeDisplacements(fileName, x);
+    std::cout << "done" << std::endl;
+
+    std::cout << "+ Printing nodal displacement solution to a file ... ";
+    v.writeDisplacements(dispFileName, x);
+    std::cout << "done" << std::endl;
+    std::cout << "+ Problem finished successfully" << std::endl;
+    std::cout << "..." << std::endl;
+    std::cout << "Check output files:" << std::endl;
+    std::string path1 = utilities::getPath();
+    std::string path2 = utilities::getPath();
+    path1 +=  meshFileName;
+    path2 +=  dispFileName;
+    std::cout << path1 << std::endl;
+    std::cout << path2 << std::endl;
+    std::cout << "*** Veamy has ended ***" << std::endl;
 }
