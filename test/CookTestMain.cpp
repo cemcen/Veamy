f8bb226e8b6410c5ef76765f8028aad3725f80e5
Alejandro Ortiz-Bernardin <aortizb@gmail.com>
2017-08-30 00:39:49 -0300

    Update CookTestMain.cpp

--- test/CookTestMain.cpp
+++ test/CookTestMain.cpp
@@ -6,27 +6,54 @@
 #include <mesher/voronoi/TriangleMeshGenerator.h>
 #include <veamy/Veamer.h>
 #include <veamy/models/constraints/values/Constant.h>
+#include <utilities/utilities.h>
 #include <veamy/physics/MaterialPlaneStrain.h>
 
 int main(){
+    std::cout << "*** Starting Veamy ***" << std::endl;
+    std::cout << "--> Test: Cook's membrane <--" << std::endl;
+    std::cout << "..." << std::endl;
+
+    // DEFINING PATH FOR THE OUTPUT FILES:
+    // If the path for the output files is not given, they are written to /home directory by default.
+    // Otherwise, include the path. For instance, for /home/user/Documents/Veamy/output.txt , the path
+    // must be "Documents/Veamy/output.txt"
+    // CAUTION: the path must exists either because it is already in your system or becuase it is created
+    // by Veamy's configuration files. For instance, Veamy creates the folder "/test" inside "/build", so
+    // one can save the output files to "/build/test/" folder, but not to "/build/test/mycustom_folder",
+    // since "/mycustom_folder" won't be created by Veamy's configuration files.
+    std::string meshFileName = "Software/Veamy-master/build/test/cook_membrane_mesh.txt";
+    std::string dispFileName = "Software/Veamy-master/build/test/cook_membrane_displacements.txt";
+    std::string geoFileName = "Software/Veamy-master/build/test/cook_membrane_geometry.txt";
+
+    std::cout << "+ Defining the domain ... ";
     std::vector<Point> TBeam_points = {Point(0,0), Point(48,44), Point(48,64), Point(0,44)};
     Region TBeam(TBeam_points);
 
-    Hole hole1 = CircularHole(Point(8,28), 5);
-    Hole hole2 = CircularHole(Point(24,38), 5);
-    Hole hole3 = CircularHole(Point(40,48), 5);
+    Hole hole1 = CircularHole(Point(8,30), 5);
+    Hole hole2 = CircularHole(Point(24,40), 4);
+    Hole hole3 = CircularHole(Point(40,50), 3);
     TBeam.addHole(hole1);
     TBeam.addHole(hole2);
     TBeam.addHole(hole3);
+    std::cout << "done" << std::endl;
 
-    TBeam.printInFile("TBeam_region.txt");
-    TBeam.generateSeedPoints(PointGenerator(functions::constantAlternating(), functions::constant()), 10, 10);
+    std::cout << "+ Printing geometry to a file ... ";
+    TBeam.printInFile(geoFileName);
+    std::cout << "done" << std::endl;
+
+    std::cout << "+ Generating polygonal mesh ... ";
+    TBeam.generateSeedPoints(PointGenerator(functions::constantAlternating(), functions::constant()), 16, 16);
     std::vector<Point> seeds = TBeam.getSeedPoints();
     TriangleMeshGenerator g(seeds, TBeam);
     PolygonalMesh mesh = g.getMesh();
-    mesh.printInFile("TBeam.txt");
+    std::cout << "done" << std::endl;
 
-    Veamer v;
+    std::cout << "+ Printing mesh to a file ... ";
+    mesh.printInFile(meshFileName);
+    std::cout << "done" << std::endl;
+
+    std::cout << "+ Defining Dirichlet and Neumann boundary conditions ... ";
     EssentialConstraints essential;
     PointSegment leftSide(Point(0,0), Point(0,44));
     SegmentConstraint left(leftSide, mesh.getPoints(), Constraint::Direction::Total, new Constant(0));
@@ -34,18 +61,42 @@ int main(){
 
     NaturalConstraints natural;
     PointSegment rightSide(Point(48,44), Point(48,64));
-    SegmentConstraint right(rightSide, mesh.getPoints(), Constraint::Direction::Vertical, new Constant(100));
+    SegmentConstraint right(rightSide, mesh.getPoints(), Constraint::Direction::Vertical, new Constant(6.25));
     natural.addConstraint(right, mesh.getPoints());
 
     ConstraintsContainer container;
     container.addConstraints(essential, mesh);
     container.addConstraints(natural, mesh);
+    std::cout << "done" << std::endl;
 
-    Material* material = new MaterialPlaneStrain(1e7, 0.3);
+    std::cout << "+ Defining linear elastic material ... ";
+    Material* material = new MaterialPlaneStrain(240, 0.3);
     ProblemConditions conditions(container, material);
+    std::cout << "done" << std::endl;
 
+    std::cout << "+ Preparing the simulation ... ";
+    Veamer v;
     v.initProblem(mesh, conditions);
+    std::cout << "done" << std::endl;
+
+    std::cout << "+ Simulating ... ";
     Eigen::VectorXd x = v.simulate(mesh);
-    std::string fileName = "CookTest.txt";
-    v.writeDisplacements(fileName, x);
+    std::cout << "done" << std::endl;
+
+    std::cout << "+ Printing nodal displacement solution to a file ... ";
+    v.writeDisplacements(dispFileName, x);
+    std::cout << "done" << std::endl;
+    std::cout << "+ Problem finished successfully" << std::endl;
+    std::cout << "..." << std::endl;
+    std::cout << "Check output files:" << std::endl;
+    std::string path1 = utilities::getPath();
+    std::string path2 = utilities::getPath();
+    std::string path3 = utilities::getPath();
+    path1 +=  meshFileName;
+    path2 +=  dispFileName;
+    path3 +=  geoFileName;
+    std::cout << path1 << std::endl;
+    std::cout << path2 << std::endl;
+    std::cout << path3 << std::endl;
+    std::cout << "*** Veamy has ended ***" << std::endl;
 }
