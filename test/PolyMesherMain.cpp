7c604a06122f67eb75a3e3a79b4e7efdce6f3f89
Alejandro Ortiz-Bernardin <aortizb@gmail.com>
2017-09-04 01:09:14 -0300

    Update PolyMesherMain.cpp

--- test/PolyMesherMain.cpp
+++ test/PolyMesherMain.cpp
@@ -3,9 +3,14 @@
 #include <veamy/physics/MaterialPlaneStrain.h>
 
 int main(){
-    std::cout << "*** Starting Veamy ***" << std::endl;
-    std::cout << "--> Test: Using a PolyMesher mesh and boundary conditions <--" << std::endl;
-    std::cout << "..." << std::endl;
+    // Set precision for plotting to output files:    
+    // OPTION 1: in "VeamyConfig::instance()->setPrecision(Precision::precision::mid)"    
+    // use "small" for 6 digits; "mid" for 10 digits; "large" for 16 digits.    
+    // OPTION 2: set the desired precision, for instance, as:    
+    // VeamyConfig::instance()->setPrecision(12) for 12 digits. Change "12" by the desired precision.    
+    // OPTION 3: Omit any instruction "VeamyConfig::instance()->setPrecision(.....)"    
+    // from this file. In this case, the default precision, which is 6 digits, will be used.   
+    VeamyConfig::instance()->setPrecision(Precision::precision::mid);  
 
     // DEFINING PATH FOR THE OUTPUT FILES:
     // If the path for the output files is not given, they are written to /home directory by default.
@@ -21,6 +26,10 @@ int main(){
     // File that contains the PolyMesher mesh and boundary conditions. Use Matlab function 
     // PolyMesher2Veamy.m to generate this file
     std::string polyMesherMeshFileName = "Software/Veamy-master/test/test_files/polymesher2veamy.txt";
+
+    std::cout << "*** Starting Veamy ***" << std::endl;
+    std::cout << "--> Test: Using a PolyMesher mesh and boundary conditions <--" << std::endl;
+    std::cout << "..." << std::endl;
 
     std::cout << "+ Defining linear elastic material ... ";
     Material* material = new MaterialPlaneStrain(1e7, 0.3);
